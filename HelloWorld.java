a25c2fe31e28974f69ba22712ea1a42c6ce5114d
riakhapekar <43671041+riakhapekar@users.noreply.github.com>
2021-03-24 18:30:48 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,10 @@ class HelloWorld{
 
   public static void main(String []args){
     System.out.println("Hello world");
+    add(10,20);
    
   }
+  public static void add(num1,num2){
+    System.out.println(num1+num2);
  
 }
